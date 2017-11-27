70e29e650d9e8cdfe0e689f35be7a5d426fb0732
majid mohamadi <m.shutdown57@gmail.com>
2017-11-27 20:50:18 +0330

    style single post page

--- src/components/blog/blogSingleComponent.vue
+++ src/components/blog/blogSingleComponent.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-        <!-- <h1>{{postDetails.title.rendered}}</h1> -->
+    <div class="direction-rtl well">
+        <h1>{{postDetails.title.rendered}}</h1>
         <div class="row">
             <div class="col-xs-4">
-                <!-- <img :src="" :alt=""> -->
+                <img class="img-responsive" :src="postDetails.img_info[0].url" :alt="postDetails.img_info[0].title">
             </div>
             <div class="col-xs-8">
-                <!-- <div v-html="postDetails.content.rendered"></div> -->
+                <div v-html="postDetails.content.rendered"></div>
             </div>
         </div>
-        {{postDetails}}
+        <br><br>
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">برچسب‌ها</h4>
+                    </div>
+                    <div class="panel-body">
+                        <a :href="category.link" v-for="category in postDetails.post_tags" class="">
+                            <span class="label label-info"> {{category.name}} </span>
+                            <span dir="ltr">&nbsp;</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">دسته‌بندی‌ها</h4>
+                    </div>
+                    <div class="panel-body">
+                        <a :href="category.link" v-for="category in postDetails.cats" class="">
+                            <span class="label label-info"> {{category.name}} </span>
+                            <span dir="ltr">&nbsp;</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- {{postDetails}} -->
     </div>
 </template>
 
@@ -59,7 +88,7 @@ export default {
         let id = this.initParam();
         this.$http.get(API_ROUTES + 'posts/' + id)
             .then((res) => {
-                // console.log(res);
+
                 // Handling Thumbnail
                 res.body.img_info = [];
                 api.getMediaId(res.body.featured_media)
@@ -85,7 +114,20 @@ export default {
                         });
                     }, reject => {
                         //
+                });
+
+                // Handling Tags
+                res.body.post_tags = [];
+                res.body.tags.map((cur_tag, i_tag, arr_tag) => {
+                    api.getTagsId(cur_tag).then(resolve => {
+                        res.body.post_tags.push({
+                            name: resolve.body.name,
+                            link: resolve.body.link
+                        });
+                    }, reject => {
+                        //
                     });
+                });
                 this.postDetails = {...res.body};
             }, (rej) => {
                 console.log(rej.status);
@@ -103,8 +145,12 @@ export default {
     direction: rtl;
 }
 
+a {
+    text-decoration: none !important;
+}
+
 img {
-    width: 128px;
-    height: 128px;
+    width: 256px;
+    height: 256px;
 }
 </style>
