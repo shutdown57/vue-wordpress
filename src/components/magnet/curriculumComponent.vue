4ae8099c96b12e6fd4097adf13c97c15b14e543c
majid mohamadi <m.shutdown57@gmail.com>
2017-11-29 16:59:31 +0330

    curriculum page handle thumbnail

--- src/components/magnet/curriculumComponent.vue
+++ src/components/magnet/curriculumComponent.vue
@@ -1,27 +1,52 @@
 <template>
-  <div>{{msg}}</div>
+  <div class="direction-rtl">
+      <h1>{{msg}}</h1>
+  </div>
 </template>
 
 <script>
 /**
  * @component: Curriculum
 */
-import {mapActions, mapState} from 'vuex';
+import api from '../../api';
+import { PRODUCT_CURRICULUM } from '../../store/staticsCategories.js';
 
 export default {
     name: 'curriculum',
     data() {
         return {
+            productCurriculum: [],
             msg: 'نمونه کار‌های برنامه‌درسی'
         };
     },
 
     created() {
-        window.document.title = 'برنامه درسی'
+        window.document.title = 'برنامه درسی';
+        let CATEGORIES_OUT = [1, 2, 7, 6];
+        api.getPostsAll(PRODUCT_CURRICULUM, CATEGORIES_OUT)
+            .then(res => {
+                if (!res.ok) {
+                    this.msg = 'مشکل در ارتباط با سرور';
+                }
+
+                // Handling Thumbnail
+                res.body.map((cur_img, i_img, arr_img) => {
+                    cur_img.img_info = [];
+                    api.getMediaId(cur_img.featured_media)
+                        .then(resolve => {
+                            cur_img.img_info.push({
+                                url: resolve.body.source_url,
+                                title: resolve.body.title.rendered
+                            });
+                        }, reject => {});
+                });
+                this.productCurriculum = res.body;
+                console.log(this.productCurriculum);
+            }, rej => { console.log(rej); });
     }
 }
 </script>
 
 <style>
-
+.direction-rtl { direction: rtl; }
 </style>
