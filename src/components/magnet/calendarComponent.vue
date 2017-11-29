cb866a3a706ea8c141c4872861069a3122d32156
majid mohamadi <m.shutdown57@gmail.com>
2017-11-29 17:00:06 +0330

    make calendar and white board page

---
+++ src/components/magnet/calendarComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+/**
+ * @component: Calendar
+ */
+import api from '../../api';
+
+export default {
+    name: 'calendar',
+    data() {
+        return {
+            msg: 'نمونه کار‌های تقویم'
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/magnet/whiteBoardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+/**
+ * @component: White Board
+ */
+import api from '../../api';
+
+export default {
+    name: 'whiteBoard',
+    data() {
+        return {
+            msg: 'نمونه کار‌های وایت برد'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
