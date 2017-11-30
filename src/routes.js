299c2be56fa0941cb271b1660700280a15bdbb03
majid mohamadi <m.shutdown57@gmail.com>
2017-11-30 13:38:02 +0330

    add other products type routes

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,13 @@ import ProductsComponent from './components/productsComponent.vue';
 import ProductsCurriculumComponent from './components/magnet/curriculumComponent.vue';
 import ProductsCalendarComponent from './components/magnet/calendarComponent.vue';
 import ProductsWhiteBoardComponent from './components/magnet/whiteBoardComponent.vue';
+import ProductsDifferentComponent from './components/magnet/differentComponent.vue';
+import ProductsMarkerComponent from './components/magnet/markerComponent.vue';
+import ProductsOnCarComponent from './components/magnet/onCarComponent.vue';
+import ProductsPictureFrameComponent from './components/magnet/pictureFrameComponent.vue';
+import ProductsReligionComponent from './components/magnet/religionComponent.vue';
+import ProductsSubscribCardComponent from './components/magnet/subscribCardComponent.vue';
+import ProductsVisitCardComponent from './components/magnet/visitCardComponent.vue';
 
 import BlogComponent from './components/blogComponent.vue';
 import BlogSingleComponent from './components/blog/blogSingleComponent.vue';
@@ -28,6 +35,13 @@ export default [
     { path: '/products/curriculum', component: ProductsCurriculumComponent, name: 'curriculum' },
     { path: '/products/calendar', component: ProductsCalendarComponent, name: 'calendar' },
     { path: '/products/white-board', component: ProductsWhiteBoardComponent, name: 'whiteBoard' },
+    { path: '/products/different', component: ProductsDifferentComponent, name: 'different' },
+    { path: '/products/marker', component: ProductsMarkerComponent, name: 'marker' },
+    { path: '/products/on-car', component: ProductsOnCarComponent, name: 'onCar' },
+    { path: '/products/picture-frame', component: ProductsPictureFrameComponent, name: 'pictureFrame' },
+    { path: '/products/religion', component: ProductsReligionComponent, name: 'religion' },
+    { path: '/products/subscrib-card', component: ProductsSubscribCardComponent, name: 'subscribCard' },
+    { path: '/products/visit-card', component: ProductsVisitCardComponent, name: 'visitCard' },
     { path: '/products', component: ProductsComponent, name: 'products' },
     
     { path: '/404', component: NotFoundComponent, name: 'notFound' }
