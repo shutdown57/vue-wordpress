24b89df7aa54f554cbb41a87b4ce948d3c894a84
majid mohamadi <m.shutdown57@gmail.com>
2017-11-30 13:35:26 +0330

    add other product types categories

--- src/store/staticsCategories.js
+++ src/store/staticsCategories.js
@@ -1,12 +1,13 @@
 
-export const PRODUCT_CATEGORIES_IN = [7, 6, 5]; // All products categories
+export const PRODUCT_CATEGORIES_IN = [7, 6, 5, 11, 16, 15, 14, 10, 12, 13]; // All products categories
 export const PRODUCT_CATEGORIES_OUT = [1, 2]; // All unproducts categories
 export const PRODUCT_WHITEBOURD = [6];
 export const PRODUCT_CALENDAR = [7];
 export const PRODUCT_CURRICULUM = [5];
-export const PRODUCT_RELIGION = [];
-export const PRODUCT_ONCAR = [];
-export const PRODUCT_MARKER = [];
-export const PRODUCT_PICTURE_FRAME = [];
-export const PRODUCT_CARD_SUBSCRIBE = [];
-export const PRODUCT_CARD_VISIT = [];
+export const PRODUCT_RELIGION = [11];
+export const PRODUCT_DIFFERENT = [16];
+export const PRODUCT_ONCAR = [15];
+export const PRODUCT_MARKER = [14];
+export const PRODUCT_PICTURE_FRAME = [10];
+export const PRODUCT_CARD_SUBSCRIBE = [12];
+export const PRODUCT_CARD_VISIT = [13];
