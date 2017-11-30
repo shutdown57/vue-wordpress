8cd99c8a0fd94d81ce77e75f1b130aabf96f7e97
majid mohamadi <m.shutdown57@gmail.com>
2017-11-30 11:36:10 +0330

    calendar page completed

--- src/store/staticsCategories.js
+++ src/store/staticsCategories.js
@@ -2,7 +2,7 @@
 export const PRODUCT_CATEGORIES_IN = [7, 6, 5]; // All products categories
 export const PRODUCT_CATEGORIES_OUT = [1, 2]; // All unproducts categories
 export const PRODUCT_WHITEBOURD = [];
-export const PRODUCT_CALENDAR = [];
+export const PRODUCT_CALENDAR = [7];
 export const PRODUCT_CURRICULUM = [5];
 export const PRODUCT_RELIGION = [];
 export const PRODUCT_ONCAR = [];
