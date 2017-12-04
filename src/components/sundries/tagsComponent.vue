e0a9579b31c134fe45be21de60656b0906f1554c
majid mohamadi <m.shutdown57@gmail.com>
2017-12-04 11:11:46 +0330

    add sundries tags component to show all tags used in website

---
+++ src/components/sundries/tagsComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>نام</th>
+                    <th>تعداد مطلب مرتبط</th>
+                    <th>شماره شناسه برچسب</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(tag, index) in tags">
+                    <td>{{index + 1}}</td>
+                    <td><router-link :to="{name: 'tagSingle', params: {tid: tag.id}}">{{tag.name}}</router-link></td>
+                    <td>{{tag.count}}</td>
+                    <td>{{tag.id}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+/**
+ * @component: Tags
+ */
+import api from '../../api';
+
+export default {
+    name: 'tags',
+
+    data() {
+        return {
+            tags: []
+        };
+    },
+
+    created() {
+        api.getTagsAll()
+            .then(res => {
+                // console.log(res);
+                this.tags = res.body;
+            }, rej => { console.log(res); });
+    }
+}
+</script>
+
+<style>
+
+</style>
