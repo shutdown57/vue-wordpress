793c049d04fc0acab6683c9347a16774f4893749
majid mohamadi <m.shutdown57@gmail.com>
2017-12-05 12:31:46 +0330

    add base url and assets url to add static images

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 // export const API_ROOT = 'http://wordpress.app/wp-json/';
 export const API_ROOT = root;
+export const BASE_URL = 'http://wordpress.app';
+export const ASSETS_PATH = BASE_URL + '/wp-content/themes/vue-new/src/assets';
 export const NAMESPACE = 'wp/v2/';
 export const API_ROUTES = API_ROOT + NAMESPACE;
 export const NONCE = nonce;
