2bf36646409b40884d620bb13fecfd76713e1809
majid mohamadi <m.shutdown57@gmail.com>
2017-12-05 12:29:37 +0330

    add image logo to home

--- src/components/homeComponent.vue
+++ src/components/homeComponent.vue
@@ -1,18 +1,45 @@
 <template>
-    <h1>{{ msg }}</h1>
+    <div>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <image-loader :src="path"
+                              width="600"
+                              height="800"
+                              class="img-responsive img-rounded"
+                              :preview="ASSETS_PATH + '/logo.png'"></image-loader>
+                <!-- <img :src="path" alt=""> -->
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 /**
  * @component: Home
 */
+import VueImageLoader from 'vue-img-loader';
+import {ASSETS_PATH} from '../config';
+
 export default {
     name: 'home',
+
+    components: {
+        'image-loader': VueImageLoader
+    },
+
     data() {
         return {
+            path: ASSETS_PATH + '/logo.png',
             msg: 'Home'
         };
     },
+
+    methods: {
+        loadImage(path) {
+            return require(path);
+        }
+    },
+
     created() {
         window.document.title = "ایرانیان مگنت";
     }
@@ -20,5 +47,5 @@ export default {
 </script>
 
 <style scoped>
-
+img { width: 100% !important; height: 100%; }
 </style>
