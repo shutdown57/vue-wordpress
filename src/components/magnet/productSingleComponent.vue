ca7db7c65a289ff496e893a6d60cf86e85e1118d
majid mohamadi <m.shutdown57@gmail.com>
2017-12-07 11:33:29 +0330

    single product view

--- src/components/magnet/productSingleComponent.vue
+++ src/components/magnet/productSingleComponent.vue
@@ -1,5 +1,39 @@
 <template>
-    <div></div>
+<div class="direction-rtl">
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title">{{msg.title.rendered}}</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div v-for="product in products" class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail" data-toggle="modal" data-target="#myModal">
+                    <img :src="product.img_info[0].url" :alt="product.img_info[0].title" v-model="img_info=product.img_info[0]">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" dir="rtl" id="myModalLabel">{{img_info.title}}</h4>
+        </div>
+        <div class="modal-body text-center">
+            <img class="img-responsive" :src="img_info.url" :alt="img_info.title">
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">بستن</button>
+        </div>
+        </div>
+    </div>
+    </div>
+        
+</div>
 </template>
 
 <script>
@@ -22,7 +56,11 @@ export default {
             return pid;
         },
         removeArray(arr, el) {
-            return arr.filter(e => e !== el);
+            // return arr.filter(e => e !== el);
+            let index = arr.indexOf(el);
+            if (index != -1) {
+                arr.splice(index, 1);
+            }
         },
         addArray(el) {
             let arr = [];
@@ -33,7 +71,8 @@ export default {
     
     data() {
         return {
-            product: {}
+            products: [],
+            msg: {}
         };
     },
 
@@ -43,11 +82,26 @@ export default {
         id = this.addArray(parseInt(id));
         let allCategories = [...PRODUCT_CATEGORIES_IN, ...[1, 2, 17]];
         // Remove id from allCategories array
-        allCategories = this.removeArray(allCategories, id);
+        this.removeArray(allCategories, id[0]);
+        // console.log(allCategories);
+        // console.log(id);
         api.getPostsAll(id, allCategories)
             .then(res => {
                 console.log(res);
-            }, rej => { console.error(rej); });
+                // Handling Thumbnail
+                res.body.map((cur_post, i_post, arr_post) => {
+                    cur_post.img_info = [];
+                    api.getMediaId(cur_post.featured_media)
+                        .then(resolve => {
+                            cur_post.img_info.push({
+                                url: resolve.body.source_url,
+                                title: resolve.body.title.rendered
+                            });
+                        }, reject => { /*console.log(reject);*/ });
+                this.products = res.body;
+                this.msg = {...this.products[0]};
+                });
+            }, rej => { /*console.error(rej);*/ });
     }
 }
 </script>
