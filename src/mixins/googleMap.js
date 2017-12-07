fe5373d199cbaae1b44aacbd589dd2bf5e7c343d
majid mohamadi <m.shutdown57@gmail.com>
2017-12-07 14:04:01 +0330

    add google map mixin to import into the about page

---
+++ src/mixins/googleMap.js
@@ -0,0 +1,12 @@
+export default {
+    data() {
+        return {
+            center: { lat: 0.0, lng: 0.0 },
+            markers: [
+                {
+                    position: { lat: 0.0, lng: 0.0 }
+                }
+            ]
+        };
+    }
+}
