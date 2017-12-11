3d81aa0f804b1903dc6ede2eea463e1c07e45cf6
majid mohamadi <m.shutdown57@gmail.com>
2017-12-11 12:56:47 +0330

    add social sharing plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import * as VueGoogleMaps from 'vue2-google-maps';
+import SocialSharing from 'vue-social-sharing';
 
 import App from './App.vue';
 import Routes from './routes';
@@ -11,6 +12,7 @@ import {GMAPS_OPT} from './config';
 Vue.use(VueResource);
 Vue.use(VueRouter);
 Vue.use(VueGoogleMaps, GMAPS_OPT);
+Vue.use(SocialSharing);
 
 const router = new VueRouter({
   routes: Routes,
