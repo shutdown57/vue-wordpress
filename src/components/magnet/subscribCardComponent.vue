4302efdb3783356d256994c291711e0115381d7f
majid mohamadi <m.shutdown57@gmail.com>
2017-12-13 11:11:22 +0330

    add infinite loading

--- src/components/magnet/subscribCardComponent.vue
+++ src/components/magnet/subscribCardComponent.vue
@@ -16,7 +16,7 @@
     </div>
         
     <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" v-if="img_info">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
         <div class="modal-header">
@@ -32,7 +32,16 @@
         </div>
     </div>
     </div>
-        
+    
+    <!-- Infinite Loading -->
+    <infinite-loading @infinite="infiniteHandler">
+        <span slot="no-more">
+            <div class="alert alert-warning alert-dismissible" role="alert" dir="rtl">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <strong>توجه!</strong> پست دیگری وجود ندارد.
+            </div>
+        </span>
+    </infinite-loading>
 </div>
 </template>
 
@@ -40,13 +49,21 @@
 /**
  * @component: White Board
  */
+import InfiniteLoadnig from 'vue-infinite-loading';
+
 import api from '../../api';
 import {PRODUCT_CARD_SUBSCRIBE} from '../../store/staticsCategories';
 
 export default {
     name: 'whiteBoard',
+
+    components: {
+        InfiniteLoadnig
+    },
+
     data() {
         return {
+            img_info: {},
             productSubscribCard: [],
             msg: 'نمونه کار‌های کارت اشتراک'
         }
@@ -54,27 +71,46 @@ export default {
 
     created() {
         window.document.title = 'کارت اشتراک مگنتی';
+    },
 
-        let CATEGORIES_OUT = [1, 2, 7, 5, 11, 16, 15, 14, 10, 13];
-        api.getPostsAll(PRODUCT_WHITEBOURD, CATEGORIES_OUT)
-            .then(res => {
-                if (!res.ok) {
-                    this.msg = 'مشکل در ارتباط با سرور';
+    methods: {
+        infiniteHandler($state) {
+            let CATEGORIES_OUT = [1, 2, 7, 5, 11, 16, 15, 14, 10, 13];
+            this.$http.get("http://wordpress.app/wp-json/wp/v2/posts", {
+                params: {
+                    categories: PRODUCT_CARD_SUBSCRIBE,
+                    categories_exclude: CATEGORIES_OUT,
+                    page: this.productSubscribCard.length / 10 + 1,
+                    per_page: 6
                 }
-
-                // Handling Thumbnail
-                res.body.map((cur_img, i_img, arr_img) => {
-                    cur_img.img_info = [];
-                    api.getMediaId(cur_img.featured_media)
-                        .then(resolve => {
-                            cur_img.img_info.push({
-                                url: resolve.body.source_url,
-                                title: resolve.body.title.rendered
-                            });
-                        }, reject => { /*console.error(reject);*/ });
-                });
-                this.productSubscribCard = res.body;
-            }, rej => { /*console.error(reject);*/ });
+            }).then(res => {
+                if (res.body.length) {
+                    // Handling Thumbnail
+                    res.body.map((cur_img, i_img, arr_img) => {
+                        cur_img.img_info = [];
+                        api.getMediaId(cur_img.featured_media)
+                            .then(resolve => {
+                                // console.log(resolve);
+                                cur_img.img_info.push({
+                                    title: resolve.body.title.rendered,
+                                    url: resolve.body.source_url
+                                });
+                            }, reject => { /*console.error(reject);*/ });
+                    });
+                    this.productSubscribCard = this.productSubscribCard.concat(res.body);
+                    console.log(this.productSubscribCard);
+                    $state.loaded();
+                    if (this.productSubscribCard.length % 10 === 0) {
+                        $state.complete();
+                    }
+                } else {
+                    $state.complete();
+                }
+            }, rej => {
+                // console.log(rej);
+                $state.complete();
+            });
+        }
     }
 }
 </script>
