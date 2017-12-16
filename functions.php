c8d2c653c8232ae8a4520a79c61e62be2b5002e5
majid mohamadi <m.shutdown57@gmail.com>
2017-12-16 14:22:44 +0330

    cookie handling, get current user, try to make wordpress ajax object, send auth cookie filter

--- functions.php
+++ functions.php
@@ -19,6 +19,23 @@
 //         </script>';
 // }
 
+/**
+ * Set cooie in browser
+ */
+// function my_update_cookie( $logged_in_cookie ){
+//     $_COOKIE[LOGGED_IN_COOKIE] = $logged_in_cookie;
+// }
+// add_action( 'set_logged_in_cookie', 'my_update_cookie' );
+
+
+add_filter( 'rest_pre_dispatch', 'prefix_return_current_user' );
+
+function prefix_return_current_user( $result ) {
+    $result = wp_get_current_user();
+    $user_id = get_current_user_id();
+    wp_set_current_user( $user_id );
+    // return $result;
+}
 
 function vue_iranian_init() {
 
@@ -26,6 +43,9 @@ function vue_iranian_init() {
     $base_path = rtrim( parse_url( $base_url, PHP_URL_PATH ), '/');
 
     // wp_enqueue_script( 'vue-iranian', get_template_directory_uri() . '/dist/build.js', array(), '1.0.0', true );
+
+    wp_register_script('ajax-main', plugins_url('/src/main.js', __FILE__));
+
     wp_localize_script( 'vue-iranian', 'ajaxPaths', array(
         'url' => admin_url( 'admin-ajax.php' ),
         'root'       => esc_url_raw( rest_url() ),
@@ -34,10 +54,15 @@ function vue_iranian_init() {
         'nonce'      => wp_create_nonce( 'wp_vue' ), // Choose a good key for nonce
         'site_name'  => get_bloginfo( 'name' )
     ) );
+    wp_enqueue_script( 'ajax-main');
 }
 
 // add_action( 'wp_enqueue_scripts', 'vue_iranian_init' );
 
+/**
+ * Filters
+ */
+apply_filters( 'send_auth_cookies', true );
 
 /**
  * Theme Supports
