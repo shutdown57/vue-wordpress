e2df6995f1415e5f7001ad7bf71ff9a3692aceae
majid mohamadi <m.shutdown57@gmail.com>
2017-12-24 13:54:01 +0330

    make user class to save user info globaly

---
+++ src/mixins/user.js
@@ -0,0 +1,29 @@
+export default {
+    __name: '',
+    __username: '',
+    __token: '',
+    __email: '',
+    
+    getToken() {
+        return this.__token;
+    },
+
+    setUserInfo(name, username, token, email) {
+        this.__name = name;
+        this.__token = token;
+        this.__email = email;
+        this.__username = username;
+    },
+
+    getName() {
+        return this.__name;
+    },
+
+    getEmail() {
+        return this.__email;
+    },
+
+    getUsername() {
+        return this.__username;
+    }
+};
