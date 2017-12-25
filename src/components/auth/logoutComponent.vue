41a2ac5881cc9ff6d98b9cb2d0869780fd8ab0f6
majid mohamadi <m.shutdown57@gmail.com>
2017-12-25 16:56:32 +0330

    make logout component

---
+++ src/components/auth/logoutComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div v-if="alert_msg" class="alert alert-danger text-center" role="alert">
+            {{ alert_msg }}
+        </div>
+    </div>
+</template>
+
+
+<script>
+/**
+ * @component: Logout
+ */
+import User from '../../mixins/user';
+
+export default {
+    name: 'logout',
+
+    data() {
+        return {
+            alert_msg: 'شما از اکانت خود خارج شدید'
+        };
+    },
+
+    created() {
+        User.clearData();
+        setTimeout(() => {
+            this.$router.push({name: 'home'});
+        }, 2000);
+    }
+}
+</script>
