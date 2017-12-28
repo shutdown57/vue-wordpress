443712cd0059f3db3d63befe5f428a1dd4303efa
majid mohamadi <m.shutdown57@gmail.com>
2017-12-28 09:57:39 +0330

    use local storage to save user data, alert message success class added

--- src/components/auth/logoutComponent.vue
+++ src/components/auth/logoutComponent.vue
@@ -18,15 +18,34 @@ export default {
 
     data() {
         return {
-            alert_msg: 'شما از اکانت خود خارج شدید'
+            alert_msg: 'شما از اکانت خود خارج شدید',
+            isLogged: this.checkToken()
         };
     },
 
     created() {
         User.clearData();
+        this.isLogged = this.checkToken();
+        this.logout();
         setTimeout(() => {
             this.$router.push({name: 'home'});
         }, 2000);
+    },
+
+    methods: {
+        logout() {
+            this.$ls.remove('access_token');
+            this.isLogged = this.checkToken();
+        },
+
+        checkToken() {
+            let token = this.$ls.get('access_token');
+            if (token) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
 </script>
