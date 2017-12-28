4501e1059d0c8e953239073933a3f995c467e9b4
majid mohamadi <m.shutdown57@gmail.com>
2017-12-28 09:54:41 +0330

    add vue local storage module

--- src/main.js
+++ src/main.js
@@ -4,17 +4,19 @@ import VueResource from 'vue-resource';
 import * as VueGoogleMaps from 'vue2-google-maps';
 import SocialSharing from 'vue-social-sharing';
 import VeeValidate from 'vee-validate';
+import VueLocalStorage from 'vue-ls';
 
 import App from './App.vue';
 import Routes from './routes';
 import {store} from './store/index';
-import {GMAPS_OPT, veeConfig} from './config';
+import {GMAPS_OPT, veeConfig, LOCALSTORAGE_OPT} from './config';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 Vue.use(VueGoogleMaps, GMAPS_OPT);
 Vue.use(SocialSharing);
 Vue.use(VeeValidate, veeConfig);
+Vue.use(VueLocalStorage, LOCALSTORAGE_OPT);
 
 const router = new VueRouter({
   routes: Routes,
@@ -22,13 +24,14 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from , next) => {
-  //  console.log(to);
-    // if ((to.path === '/wp-login.php') && ())
-    if (!to.matched.length) {
-      next('/404');
-    }
-    
-    next();
+  if (!to.matched.length) {
+    next('/404');
+  }
+  console.log(from.path);
+  if (from.path === '/login' || from.path === '/logout') {
+    location.reload();
+  }
+  next();
 });
 
 new Vue({
