2c89ec1e91dcdc773ee0ee8f98436387e70bfb44
majid mohamadi <m.shutdown57@gmail.com>
2018-01-03 15:54:06 +0330

    add mail configuration and handling data from user and send mail to company

--- functions.php
+++ functions.php
@@ -72,3 +72,117 @@ add_theme_support(
     'post-formats', 
     array('aside', 'image', 'video', 'gallery', 'link', 'quote', 'status', 'chat')
  );
+
+/**
+ * PHPMAILER Configuration
+ */
+add_action( 'phpmailer_init', 'send_smtp_email' );
+function send_smtp_email($phpmailer) {
+    $phpmailer->isSMTP();
+    $phpmailer->Host       = SMTP_HOST;
+    $phpmailer->SMTPAuth   = SMTP_AUTH;
+    $phpmailer->Port       = SMTP_PORT;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->Username   = SMTP_USERNAME;
+    $phpmailer->Password   = SMTP_PASSWORD;
+    $phpmailer->From       = SMTP_FROM;
+    $phpmailer->FromName   = SMTP_FROMNAME;
+}
+
+/**
+ * Ask form handling
+ */
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'forms/v1', '/ask-question',
+    array(
+        'methods' => 'GET',
+        'callback' => 'ask_question_form'
+    ) );
+} );
+function ask_question_form( WP_REST_Request $request ) {
+    
+    $user_email = esc_attr( $request->get_param( 'email' ) );
+    $title = esc_attr( $request->get_param('title') );
+    $content = esc_attr( $request->get_param('content') );
+
+    if ( !$user_email || !$title || !$content ) {
+        $response = json_encode( array('data' => 'some filed is empty.') );
+        echo $response;
+        wp_die();
+    }
+
+    $from = 'info@iranianmagnet.com';
+    $comp_email = 'iranian.group@yahoo.com';
+
+    $to = array($user_email, $comp_email);
+
+    wp_mail( $to, $title, $content, $headers );
+    $response = json_encode(array('data' => true, 'email' => $to, 'title' => $title, 'content' => $content));
+    echo $response;
+    wp_die();
+}
+
+//add_action( 'wp_ajax_form_sign_up', 'form_sign_up' ); // ajax for logged in users
+//add_action( 'wp_ajax_nopriv_form_sign_up', 'form_sign_up' ); // ajax for not logged in users
+//function form_sign_up() {
+//    if (isset($_REQUEST) && wp_verify_nonce($_REQUEST["nonce"], "_user_nonce")) {
+//        $username = $_REQUEST["username"];
+//        $password = $_REQUEST["password"];
+//        $passconf = $_REQUEST["passconf"];
+//        $first_name = $_REQUEST["first_name"];
+//        $last_name = $_REQUEST["last_name"];
+//        $brand_name = $_REQUEST["brand_name"];
+//        $email = $_REQUEST["email"];
+//        $phone = $_REQUEST["phone"];
+//        $mobile = $_REQUEST["mobile"];
+//        $address = $_REQUEST["address"];
+//
+//        if (empty($username) || empty($password) || empty($passconf) ||
+//            empty($first_name) || empty($last_name) || empty($brand_name) ||
+//            empty($email) || empty($phone) || empty($mobile) ||
+//            ($password !== $passconf) || (empty($address))) {
+//            $result["data"] = "لطفا تمامی فیلدها را به درستی و کامل پر کنید";
+//            $result = json_encode($result);
+//            echo $result;
+//            wp_die();
+//        }
+//
+//        // Insert data into database by $wpdb
+//        global $wpdb;
+//
+//        $user = $wpdb->get_row(
+//            "SELECT * FROM wp_clients WHERE username='". $username ."'"
+//        );
+//
+//        if ($user){
+//            $result["message"] = "کاربر با این نام کاربری موجود است";
+//            $result["data"] = true;
+//            $result = json_encode($result);
+//            echo $result;
+//            wp_die();
+//        } else {
+//            $wpdb->insert('wp_clients',
+//                array(
+//                    'username' => $username,
+//                    'password' => wp_hash_password($password),
+//                    'firstname' => $first_name,
+//                    'lastname' => $last_name,
+//                    'email' => $email,
+//                    'brandname' => $brand_name,
+//                    'phone' => $phone,
+//                    'mobile' => $mobile,
+//                    'address' => $address
+//                ),
+//                array('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'));
+//            $result["data"] = "به ایرانیان مگنت خوش آمدید";
+//            $result = json_encode($result);
+//            echo $result;
+//        }
+//
+//    } else {
+//        $result["data"] = "لطفا اطلاعات را به صورت کامل وارد نمایید";
+//        $result = json_encode($result);
+//        echo $result;
+//    }
+//    wp_die();
+//}
