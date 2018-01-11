621072a3895a6f9f291083dcba2c84fcd5b65d8d
majid mohamadi <m.shutdown57@gmail.com>
2018-01-11 14:47:33 +0330

    style and fix issue for share to social networks

--- src/components/layouts/footerComponent.vue
+++ src/components/layouts/footerComponent.vue
@@ -15,7 +15,7 @@
             </div>
           </div>
           <div class="col-sm-12 col-md-4 text-center">
-            <h2>شبکه‌های اجتماعی</h2>
+            <h3>به اشتراک گذاری این صفحه</h3>
             <social-sharing
                 title="مگنت ایرانیان"
                 description="تولید کننده انواع مگنت در سایزها و قالبهای مختلف"
@@ -23,34 +23,58 @@
                 hashtags="مگنت, مگنت تبلیغاتی, ایراینان مگنت, آهیربای رویخچالی, کارت ویزیت, وایت بورد"
                 twitter-user="iranianmagent"
                 inline-template>
-              <div>
-                <network network="email">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="facebook">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="googleplus">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="linkedin">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="pinterest">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="reddit">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="skype">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="telegram">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
-                <network network="twitter">
-                  <span class="glyphicon glyphicon-envelope"></span>
-                </network>
+              <div class="text-center">
+                <div class="row">
+                  <div class="col-xs-2 col-xs-offset-3">
+                    <network network="telegram">
+                      <img class="img-responsive img-icon" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/telegram-50.png'">
+                    </network>
+                  </div>
+                  <div class="col-xs-2">
+                    <network network="twitter">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/twitter-50.png'">
+                    </network>
+                  </div>
+                  <div class="col-xs-2">
+                    <network network="reddit">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/reddit-50.png'">
+                    </network>
+                  </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-2 col-xs-offset-3">
+                    <network network="facebook">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/facebook-50.png'">
+                    </network>
+                  </div>
+                  <div class="col-xs-2">
+                    <network network="googleplus">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/google-plus-50.png'">
+                    </network>
+                  </div>
+                  <div class="col-xs-2">
+                    <network network="linkedin">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/linkedin-50.png'">
+                    </network>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-xs-2 col-xs-offset-3">
+                    <network network="pinterest">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/pinterest-50.png'">
+                    </network>
+                  </div>
+                  <div class="col-xs-2">
+                    <network network="email">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/email-50.png'">
+                    </network>
+                  </div>
+                  <div class="col-xs-2">
+                    <network network="skype">
+                      <img class="img-responsive" :src="'http://wordpress.app' + '/wp-content/themes/vue-new/src/assets' + '/share-icons/skype-50.png'">
+                    </network>
+                  </div>
+                </div>
               </div>
             </social-sharing>
           </div>
@@ -70,18 +94,72 @@
 /**
  * @component: Footer
 */
+import VueImageLoader from 'vue-img-loader';
+import {ASSETS_PATH} from '../../config';
 
 export default {
   name: 'footer',
+
+  components: {
+    'image-loader': VueImageLoader
+  },
+
   data () {
     return {
-        msg: "Footer"
+      msg: "Footer",
+      
+      shareIcons: {
+        telegram: {
+          path: ASSETS_PATH + '/share-icons/telegram-50.png',
+          name: 'telegram'
+        },
+        twitter: {
+          path: ASSETS_PATH + '/share-icons/twitter-50.png',
+          name: 'twitter'
+        },
+        email: {
+          path: ASSETS_PATH + '/share-icons/email-50.png',
+          name: 'email'
+        },
+        facebook: {
+          path: ASSETS_PATH + '/share-icons/facebook-50.png',
+          name: 'facebook'
+        },
+        googleplus: {
+          path: ASSETS_PATH + '/share-icons/google-plus-50.png',
+          name: 'googleplus'
+        },
+        linkedin: {
+          path: ASSETS_PATH + '/share-icons/linkedin-50.png',
+          name: 'linkedin'
+        },
+        pinterest: {
+          path: ASSETS_PATH + '/share-icons/pinterest-50.png',
+          name: 'pinterest'
+        },
+        reddit: {
+          path: ASSETS_PATH + '/share-icons/reddit-50.png',
+          name: 'reddit'
+        },
+        skype: {
+          path: ASSETS_PATH + '/share-icons/skype-50.png',
+          name: 'skype'
+        }
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.img-icon {
+  max-width: 20px;
+  min-width: 16px;
+  max-height: 20px;
+  min-height: 16px;
+  width: 16px;
+  height: 16px;
+}
 footer {
     /* position: absolute !important; */
     left: 0;
