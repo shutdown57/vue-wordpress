58ee3ce9e29c4f5b0d9043a1b124a44a7f7e87cd
majid mohamadi <m.shutdown57@gmail.com>
2018-01-16 11:41:18 +0330

    fix pagination, cleanup

--- src/components/magnet/whiteBoardComponent.vue
+++ src/components/magnet/whiteBoardComponent.vue
@@ -80,17 +80,15 @@ export default {
                 params: {
                     categories: PRODUCT_WHITEBOURD,
                     categories_exclude: CATEGORIES_OUT,
-                    page: this.productWhiteBoard.length / 10 + 1,
+                    page: this.productWhiteBoard.length / 6 + 1,
                     per_page: 6
                 }
             }).then(res => {
                 if (res.body.length) {
-                    // Handling Thumbnail
                     res.body.map((cur_img, i_img, arr_img) => {
                         cur_img.img_info = [];
                         api.getMediaId(cur_img.featured_media)
                             .then(resolve => {
-                                // console.log(resolve);
                                 cur_img.img_info.push({
                                     title: resolve.body.title.rendered,
                                     url: resolve.body.source_url
@@ -99,14 +97,13 @@ export default {
                     });
                     this.productWhiteBoard = this.productWhiteBoard.concat(res.body);
                     $state.loaded();
-                    if (this.productWhiteBoard.length % 10 === 0) {
+                    if (this.productWhiteBoard.length % 6 == 10) {
                         $state.complete();
                     }
                 } else {
                     $state.complete();
                 }
             }, rej => {
-                // console.log(rej);
                 $state.complete();
             });
         }
