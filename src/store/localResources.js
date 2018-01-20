33f259525e651d9f28691df4d9b624c37f2b1062
majid mohamadi <m.shutdown57@gmail.com>
2018-01-20 10:46:17 +0330

    add local resources file to cleanup static files from components

---
+++ src/store/localResources.js
@@ -0,0 +1,304 @@
+import {ASSETS_PATH} from '../config';
+
+
+export const lastMagnets = [
+    {
+        path: ASSETS_PATH + '/samples/calendar-1.png',
+        alt: 'نمونه کار تقویم مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/calendar-2.png',
+        alt: 'نمونه کار تقویم مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/calendar-3.png',
+        alt: 'نمونه کار تقویم مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/calendar-4.png',
+        alt: 'نمونه کار تقویم مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/calendar-5.png',
+        alt: 'نمونه کار تقویم مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-1.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-2.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-3.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-4.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-5.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-6.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-7.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/card-8.png',
+        alt: 'نمونه کار کارت اشتراک و یا کارت کارت ویزیت مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/curriculum-1.png',
+        alt: 'نمونه کار برنامه درسی مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/curriculum-2.png',
+        alt: 'نمونه کار برنامه درسی مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/curriculum-3.png',
+        alt: 'نمونه کار برنامه درسی مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/curriculum-4.png',
+        alt: 'نمونه کار برنامه درسی مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/curriculum-5.png',
+        alt: 'نمونه کار برنامه درسی مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/marker-1.png',
+        alt: 'نمونه ماژیک مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/marker-2.png',
+        alt: 'نمونه ماژیک مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/on-car-1.png',
+        alt: 'نمونه کار مگنت روی خودرو'
+    },
+    {
+        path: ASSETS_PATH + '/samples/on-car-2.png',
+        alt: 'نمونه کار مگنت روی خودرو'
+    },
+    {
+        path: ASSETS_PATH + '/samples/on-car-3.png',
+        alt: 'نمونه کار مگنت روی خودرو'
+    },
+    {
+        path: ASSETS_PATH + '/samples/on-car-4.png',
+        alt: 'نمونه کار مگنت روی خودرو'
+    },
+    {
+        path: ASSETS_PATH + '/samples/picture-frame-1.png',
+        alt: 'نمونه کار قاب عکس مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/picture-frame-2.png',
+        alt: 'نمونه کار قاب عکس مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/picture-frame-3.png',
+        alt: 'نمونه کار قاب عکس مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/picture-frame-4.png',
+        alt: 'نمونه کار قاب عکس مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/picture-frame-5.png',
+        alt: 'نمونه کار قاب عکس مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/religion-1.png',
+        alt: 'نمونه کار مگنت مذهبی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/religion-2.png',
+        alt: 'نمونه کار مگنت مذهبی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/religion-3.png',
+        alt: 'نمونه کار مگنت مذهبی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/religion-4.png',
+        alt: 'نمونه کار مگنت مذهبی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/religion-5.png',
+        alt: 'نمونه کار مگنت مذهبی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-1.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-2.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-3.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-4.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-5.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-6.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-7.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-8.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-9.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-10.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-11.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+    {
+        path: ASSETS_PATH + '/samples/white-board-12.png',
+        alt: 'نمونه کار وایت برد مگنتی'
+    },
+];
+
+export const thumbnails = {
+    curriculum: {
+        path: ASSETS_PATH + '/home-page/برنامه درسی.png',
+        linkObject: {
+            name: 'curriculum'
+        }
+    },
+    calendar: {
+        path: ASSETS_PATH + '/home-page/تقویم.png',
+        linkObject: {
+            name: 'calendar'
+        }
+    },
+    onCar: {
+        path: ASSETS_PATH + '/home-page/روی خودرو.png',
+        linkObject: {
+            name: 'onCar'
+        }
+    },
+    pictureFrame: {
+        path: ASSETS_PATH + '/home-page/قاب عکس.png',
+        linkObject: {
+            name: 'pictureFrame'
+        }
+    },
+    subscribeCard: {
+        path: ASSETS_PATH + '/home-page/کارت اشتراک.png',
+        linkObject: {
+            name: 'subscribCard'
+        }
+    },
+    marker: {
+        path: ASSETS_PATH + '/home-page/ماژیک.png',
+        linkObject: {
+            name: 'marker'
+        }
+    },
+    religion: {
+        path: ASSETS_PATH + '/home-page/مذهبی.png',
+        linkObject: {
+            name: 'religion'
+        }
+    },
+    whiteBoard: {
+        path: ASSETS_PATH + '/home-page/وایت برد.png',
+        linkObject: {
+            name: 'whiteBoard'
+        }
+    }
+};
+
+export const progress = {
+    img1: {
+        title: 'مشاوره',
+        path: ASSETS_PATH + '/home-page/مشاوره.png',
+        alt: 'مشاوره',
+        description: 'مشاوره به صورت کاملا رایگان و در تمامی زمینه‌های تبلیغاتی'
+    },
+    
+    img2: {
+        title: 'عکاسی',
+        path: ASSETS_PATH + '/home-page/عکاسی.png',
+        alt: 'آتلیه عکاسی',
+        description: 'تیم عکاسی مجرب مجهز به آتلیه همراه, آماده همکاری'
+    },
+    
+    img3: {
+        title: 'طراحی',
+        path: ASSETS_PATH + '/home-page/طراحی.png',
+        alt: 'طراحان مجرب',
+        description: 'طراحان ما در با سابقه بیش از ۱۰ سال در زمینه طراحی انواع مگنت'
+    },
+    
+    img4: {
+        title: 'چاپ',
+        path: ASSETS_PATH + '/home-page/چاپ.png',
+        alt: 'چاپ افست',
+        description: 'چاپ توسط دستگاه‌های افست چهار رنگ انجام میشود.'
+    },
+};
+
+export const brands = [
+    ASSETS_PATH + '/brands/Ab-o-Fazelab.png',
+    ASSETS_PATH + '/brands/bamilo.png',
+    ASSETS_PATH + '/brands/Bosch.png',
+    ASSETS_PATH + '/brands/Butane.png',
+    ASSETS_PATH + '/brands/daewoo.png',
+    ASSETS_PATH + '/brands/daland.png',
+    ASSETS_PATH + '/brands/Delpazir.png',
+    ASSETS_PATH + '/brands/Deo.png',
+    ASSETS_PATH + '/brands/Draje.png',
+    ASSETS_PATH + '/brands/fda.png',
+    ASSETS_PATH + '/brands/hamrah-aval.png',
+    ASSETS_PATH + '/brands/Iran-Insurance.png',
+    ASSETS_PATH + '/brands/iran-khodro.png',
+    ASSETS_PATH + '/brands/iranradiator.png',
+    ASSETS_PATH + '/brands/isoco.png',
+    ASSETS_PATH + '/brands/Kourosh.png',
+    ASSETS_PATH + '/brands/lg.png',
+    ASSETS_PATH + '/brands/Majid-Logo.png',
+    ASSETS_PATH + '/brands/mihan.png',
+    ASSETS_PATH + '/brands/mokhaberat.png',
+    ASSETS_PATH + '/brands/Nestle-waterLogo.png',
+    ASSETS_PATH + '/brands/novin.png',
+    ASSETS_PATH + '/brands/Oghaf.png',
+    ASSETS_PATH + '/brands/pegah.png',
+    ASSETS_PATH + '/brands/Rahvar.png',
+    ASSETS_PATH + '/brands/samsung.png',
+    ASSETS_PATH + '/brands/shahrdari-tehran.png',
+    ASSETS_PATH + '/brands/shahrvand.png',
+    ASSETS_PATH + '/brands/snowa.png',
+    ASSETS_PATH + '/brands/Sunstar(FA)Logo.png',
+    ASSETS_PATH + '/brands/Taje.png'
+];
