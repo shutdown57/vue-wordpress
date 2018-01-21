4d4d641293dabd83d68dba6a65ea58cb8c605b65
majid mohamadi <m.shutdown57@gmail.com>
2018-01-21 10:34:10 +0330

    +

---
+++ src/components/user/profileComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>User Profile
+    </div>
+</template>
+
+<script>
+/**
+ * Profile
+ */
+export default {
+    name: 'profile',
+
+    data() {
+        return {
+            user_: {}
+        };
+    },
+
+    methods: {
+        getUser() {
+            let user = this.$ls.get('info', false);
+            if (user) {
+                this.user_ = {...user};
+            }
+        }
+    },
+
+    mounted() {
+        this.getUser();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
