067c28dc6c2114ca195f9d67ef7d8615308b395b
majid mohamadi <m.shutdown57@gmail.com>
2018-01-23 17:02:38 +0330

    add user info, style user info

--- src/components/user/profileComponent.vue
+++ src/components/user/profileComponent.vue
@@ -1,5 +1,47 @@
 <template>
-    <div>User Profile
+    <div class="container">
+        <h1 dir="rtl">اطلاعات شخصی</h1>
+        <br>
+        <div dir="rtl" class="row">
+            <div class="col-md-6 col-sd-12 col-md-push-6">
+                <div class="form-group">
+                    <label for="name">نام و نام خانوادگی</label>
+                    <input dir="rtl" class="form-control" type="text" 
+                        id="name" name="name" :value="user_.__name" :disabled="disable.name">
+                </div>
+            </div>
+            <div class="col-md-6 col-sd-12 col-md-pull-6">
+                <div class="form-group">
+                    <label for="name">ایمیل</label>
+                    <input dir="ltr" class="form-control" type="text" 
+                        id="name" name="name" :value="user_.__email" :disabled="disable.email">
+                </div>
+            </div>
+            <div class="col-md-6 col-sd-12 col-md-push-6">
+                <div class="form-group">
+                    <label for="name">نام کاربری</label>
+                    <input dir="ltr" class="form-control" type="text" 
+                        id="name" name="name" :value="user_.__username" :disabled="disable.username">
+                </div>
+            </div>
+            <div class="col-md-6 col-sd-12 col-md-pull-6">
+                <div class="form-group">
+                    <label for="name">نام کاربری</label>
+                    <input dir="ltr" class="form-control" type="text" 
+                        id="name" name="name" :value="user_.__username" :disabled="disable.default">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 col-md-offset-4">
+                <br>
+                <button class="btn btn-primary btn-block" @click="editInfo">ویرایش</button>
+            </div>
+            <div class="col-md-2">
+                <br>
+                <button class="btn btn-success btn-block" :disabled="disable.accept">تایید</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,21 +54,44 @@ export default {
 
     data() {
         return {
-            user_: {}
+            user_: {},
+            disable: {
+                default: true,
+                name: true,
+                email: true,
+                username: true,
+                accept: true
+            }
         };
     },
 
+    // computed: {
+    //     editInfo: function () {
+    //         this.disable.name = !this.disable.name;
+    //         this.disable.email = !this.disable.email;
+    //         this.disable.username = !this.disable.username;
+    //         this.disable.accept = !this.disable.accept;
+    //     }
+    // },
+
     methods: {
+        editInfo() {
+            this.disable.name = !this.disable.name;
+            this.disable.email = !this.disable.email;
+            this.disable.username = !this.disable.username;
+            this.disable.accept = !this.disable.accept;
+        },
         getUser() {
             let user = this.$ls.get('info', false);
             if (user) {
                 this.user_ = {...user};
             }
+            
         }
     },
 
     created() {
-        window.document.title = 'صفحه شخصی کاربر';
+        window.document.title = 'اطلاعات شخصی';
     },
 
     mounted() {
@@ -36,5 +101,5 @@ export default {
 </script>
 
 <style scoped>
-
+.width-200 { width: 200% !important; }
 </style>
