670d78da641908c403b501eb0b9f6eff5908b2fe
majid mohamadi <m.shutdown57@gmail.com>
2018-01-27 17:31:10 +0330

    get user orders from web

--- src/components/user/sentFormsComponent.vue
+++ src/components/user/sentFormsComponent.vue
@@ -6,11 +6,34 @@
 /**
  * Sent Forms
  */
+import {NONCE} from '../../config';
+
 export default {
     name: 'sentForms',
 
+    methods: {
+        getData() {
+            let user = this.$ls.get('info');
+            console.log(user.__token);
+            console.log(NONCE);
+            this.$http.get('http://wordpress.app/wp-json/forms/v1/orders', {
+                params: {
+                    user_email: user.__email
+                },
+                before: (request) => {
+                    request.headers.set('X-WP-Nonce', NONCE);
+                    request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+                    request.headers.set('Authorization', 'Basic ' + user.__token);
+                }
+            }).then((resp) => {
+                console.log(resp);
+            }, (err) => { /*console.error(err);*/ });
+        }
+    },
+
     created() {
         window.document.title = 'لیست فرم‌های ارسال شده';
+        this.getData();
     }
 }
 </script>
