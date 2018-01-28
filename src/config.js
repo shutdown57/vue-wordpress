7e59940e3b757b1262a72278e6ff30506113c723
majid mohamadi <m.shutdown57@gmail.com>
2018-01-28 15:41:50 +0330

    clean unneeded info

--- src/config.js
+++ src/config.js
@@ -1,18 +1,17 @@
-// export const API_ROOT = 'http://wordpress.app/wp-json/';
 
 export const API_ROOT = root;
-export const BASE_URL = 'http://wordpress.app';
+export const BASE_URL = 'http://wordpress.app'; // Main url
 export const ASSETS_PATH = BASE_URL + '/wp-content/themes/vue-new/src/assets';
 export const NAMESPACE = 'wp/v2/';
 export const API_ROUTES = API_ROOT + NAMESPACE;
 export const NONCE = nonce;
 export const CookieDomain = 'wordpress.app';
 
-export const APP_PASS = 'Lnru goL1 8wTD Bgrd m6UB k25W';
+export const APP_PASS = ''; // App password
 
 export const GMAPS_OPT = {
     load: {
-        key: 'AIzaSyA7A9AFSDRwdFPj7OC_pImF7zpeOu3D6mw', // GOOGLE API KEY
+        key: '', // GOOGLE API KEY
         v: '3.29'    // GOOGLE API VERSION
     }
 };
