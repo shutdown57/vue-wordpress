70db39ad352c51454af2bb13ef38c90bf80d43a9
majid mohamadi <m.shutdown57@gmail.com>
2018-01-28 11:07:33 +0330

    add teble and fetch data from server

--- src/components/user/sentFormsComponent.vue
+++ src/components/user/sentFormsComponent.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>Sent Forms</div>
+    <div>
+        <table class="table table-bordered table-hover table-striped table-xs-block">
+            <tr class="head-bg">
+                <th>#</th>
+                <th>نام</th>
+                <th>عنوان</th>
+                <th>توضیحات</th>
+                <th>نوع سفارش</th>
+                <th>تیراژ</th>
+                <th>اندازه</th>
+                <th>تاریخ سفارش</th>
+            </tr>
+            <tr v-for="(order, index) in orders">
+                <td>{{index}}</td>
+                <td>{{order.name}}</td>
+                <td>{{order.title}}</td>
+                <td>{{order.description}}</td>
+                <td>{{order.product_type}}</td>
+                <td>{{order.circulation}}</td>
+                <td>{{order.size}}</td>
+                <td>{{order.date}}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -11,22 +34,30 @@ import {NONCE} from '../../config';
 export default {
     name: 'sentForms',
 
+    data() {
+        return {
+            user: {},
+            orders: []
+        };
+    },
+
     methods: {
         getData() {
-            let user = this.$ls.get('info');
-            console.log(user.__token);
-            console.log(NONCE);
+            this.user = this.$ls.get('info');
+            // console.log(user.__token);
+            // console.log(NONCE);
             this.$http.get('http://wordpress.app/wp-json/forms/v1/orders', {
                 params: {
-                    user_email: user.__email
+                    user_email: this.user.__email
                 },
                 before: (request) => {
                     request.headers.set('X-WP-Nonce', NONCE);
                     request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
-                    request.headers.set('Authorization', 'Basic ' + user.__token);
+                    request.headers.set('Authorization', 'Basic ' + this.user.__token);
                 }
             }).then((resp) => {
                 console.log(resp);
+                this.orders = [...resp.body.data.orders];
             }, (err) => { /*console.error(err);*/ });
         }
     },
@@ -39,5 +70,5 @@ export default {
 </script>
 
 <style scoped>
-
+.head-bg { background-color: aliceblue; }
 </style>
