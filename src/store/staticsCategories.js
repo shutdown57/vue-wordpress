f34e99fefefb9de8a2fafb119b0d631db657d66a
majid mohamadi <m.shutdown57@gmail.com>
2018-01-28 15:40:49 +0330

    add comments

--- src/store/staticsCategories.js
+++ src/store/staticsCategories.js
@@ -1,15 +1,15 @@
 
 export const PRODUCT_CATEGORIES_IN = [7, 6, 5, 11, 16, 15, 14, 10, 12, 13]; // All products categories
 export const PRODUCT_CATEGORIES_OUT = [1, 2]; // All unproducts categories
-export const PRODUCT_WHITEBOURD = [6];
-export const PRODUCT_CALENDAR = [7];
-export const PRODUCT_CURRICULUM = [5];
-export const PRODUCT_RELIGION = [11];
-export const PRODUCT_DIFFERENT = [16];
-export const PRODUCT_ONCAR = [15];
-export const PRODUCT_MARKER = [14];
-export const PRODUCT_PICTURE_FRAME = [10];
-export const PRODUCT_CARD_SUBSCRIBE = [12];
-export const PRODUCT_CARD_VISIT = [13];
+export const PRODUCT_WHITEBOURD = [6]; // White boards
+export const PRODUCT_CALENDAR = [7]; // Calendars
+export const PRODUCT_CURRICULUM = [5]; // Curriculums
+export const PRODUCT_RELIGION = [11]; // Religions
+export const PRODUCT_DIFFERENT = [16]; // Differents
+export const PRODUCT_ONCAR = [15]; // On cars
+export const PRODUCT_MARKER = [14]; // Markers
+export const PRODUCT_PICTURE_FRAME = [10]; // Picture frames
+export const PRODUCT_CARD_SUBSCRIBE = [12]; // Subscribe cards
+export const PRODUCT_CARD_VISIT = [13]; // Visit cards
 
-export const BLOG_CATEGORY = [17];
+export const BLOG_CATEGORY = [17]; // Blogs
