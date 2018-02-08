416e52110fb10662a7fbaea89ba4bae6fa3d7f7a
majid mohamadi <m.shutdown57@gmail.com>
2018-02-08 10:29:36 +0330

    cleanup

--- src/components/layouts/googleMapsComponent.vue
+++ src/components/layouts/googleMapsComponent.vue
@@ -15,7 +15,6 @@
                     :title="'شرکت ایرانیان مگنت'"
                     ></gmap-marker>
         </gmap-map>
-        <!-- <div id="map"></div> -->
     </div>
 </template>
 
@@ -23,8 +22,6 @@
 /**
  * @component: Google Maps
  */
-
-
 export default {
     name: 'googleMaps',
 
@@ -43,69 +40,15 @@ export default {
         updateCenter(center) {
             this.position = { lat: center.lat, lng: center.lng };
         }
-        // initMap() {
-        //     this.map = new google.maps.Map(document.getElementById('map'), {
-        //         center: { lat: 35.7012317, lng: 51.4443317 },
-        //         zoom: 16,
-        //         disableDefaultUI: true
-        //     });
-        // }
     },
 
     mounted() {
-        // setTimeout(() => {
-        //     this.$refs.map.$mapCreated.then(res => {
-        //         // console.log(res);
-        //         this.mapLoaded = true;
-        //     }, rej => { console.log(rej); });
-        // }, 5000);
-        // this.initMap();
-        // this.$refs.map.$mapCreated.then(res => {
-        //     // console.log(res);
-        //     this.mapLoaded = true;
-        // }, rej => { console.log(rej); });
         this.$refs.map.$mapCreated.then(res => {
-            // console.log(this.$refs.map);
             this.mapLoaded = true;
-            // google.maps.event.addListenerOnce(this.$refs.map, 'idle', function(){
-            //     google.maps.event.trigger(this.$refs.map, 'resize');
-            //     this.$refs.map.checkResize();
-            //     this.$refs.map.setCenter(this.position);
-            //     this.$refs.map.setZoom( this.$refs.map.getZoom() );
-            //     console.log("OK");
-            // });
             google.maps.event.trigger(this.$refs.map, 'resize');
-            // this.$refs.map.checkResize();
-            // this.$refs.map.setCenter(this.position);
-            // this.$refs.map.setZoom( this.$refs.map.getZoom() );
             this.$refs.map.resize();
             this.$refs.map.resizePreserveCenter();
         }, rej => { console.log(rej); });
-    },
-
-    updated() {
-        // setInterval(() => {
-        //     this.$refs.map.$mapCreated.then(res => {
-        //         // console.log(res);
-        //         this.mapLoaded = true;
-        //         google.maps.event.addListenerOnce(this.$refs.map, 'idle', function(){
-        //             google.maps.event.trigger(this.$refs.map, 'resize');
-        //             this.$refs.map.checkResize();
-        //             this.$refs.map.setCenter(this.position);
-        //             this.$refs.map.setZoom( this.$refs.map.getZoom() );
-        //             console.log("OK");
-        //         });
-        //     }, rej => { console.log(rej); });
-        // }, 2000);
-        // setTimeout(() => {
-        //     // this.$refs.map.resizePreserveCenter(this.position);
-        //     google.maps.event.addListenerOnce(this.$refs.map, 'idle', function(){
-        //         google.maps.event.trigger(this.$refs.map, 'resize');
-        //         this.$refs.map.checkResize();
-        //         // this.$refs.map.setCenter(this.position);
-        //         this.$refs.map.setZoom( this.$refs.map.getZoom() );
-        //     });
-        // }, 2000);
     }
 }
 </script>
