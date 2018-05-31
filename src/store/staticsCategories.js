dca03ae14341e63c14689aa1d6b43c1cdaa571b5
majid mohamadi <m.shutdown57@gmail.com>
2018-05-31 13:42:09 +0430

    fixed category numbers

--- src/store/staticsCategories.js
+++ src/store/staticsCategories.js
@@ -1,15 +1,53 @@
 
-export const PRODUCT_CATEGORIES_IN = [7, 6, 5, 11, 16, 15, 14, 10, 12, 13]; // All products categories
-export const PRODUCT_CATEGORIES_OUT = [1, 2]; // All unproducts categories
-export const PRODUCT_WHITEBOURD = [6]; // White boards
-export const PRODUCT_CALENDAR = [7]; // Calendars
-export const PRODUCT_CURRICULUM = [5]; // Curriculums
-export const PRODUCT_RELIGION = [11]; // Religions
-export const PRODUCT_DIFFERENT = [16]; // Differents
-export const PRODUCT_ONCAR = [15]; // On cars
-export const PRODUCT_MARKER = [14]; // Markers
-export const PRODUCT_PICTURE_FRAME = [10]; // Picture frames
-export const PRODUCT_CARD_SUBSCRIBE = [12]; // Subscribe cards
-export const PRODUCT_CARD_VISIT = [13]; // Visit cards
+export const PRODUCT_CATEGORIES_IN = [83, 84, 85, 86, 87, 88, 89, 90, 91, 92]; // All products categories
+export const PRODUCT_CATEGORIES_OUT = [1, 61]; // All unproducts categories
 
-export const BLOG_CATEGORY = [17]; // Blogs
+export const PRODUCT_WHITEBOURD = [92]; // White boards
+export const PRODUCT_CALENDAR = [83]; // Calendars
+export const PRODUCT_CURRICULUM = [84]; // Curriculums
+export const PRODUCT_RELIGION = [89]; // Religions
+export const PRODUCT_DIFFERENT = [85]; // Differents
+export const PRODUCT_ONCAR = [87]; // On cars
+export const PRODUCT_MARKER = [86]; // Markers
+export const PRODUCT_PICTURE_FRAME = [88]; // Picture frames
+export const PRODUCT_CARD_SUBSCRIBE = [90]; // Subscribe cards
+export const PRODUCT_CARD_VISIT = [91]; // Visit cards
+
+export const BLOG_CATEGORY = [61]; // Blogs
+
+/*******************************************************************************************************/
+
+const ALL_CATEGORIES = [61, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 1];
+
+/** Categories
+ * 
+ * blog 61
+ * calendar 83
+ * curriculum 84
+ * diff 85
+ * marker 86
+ * on car 87
+ * picture 88
+ *          single XX
+ * religion 89
+ * subscrib 90
+ * visit 91
+ * white 92
+ * uncategorize 1
+ */
+
+ /** Tags
+  * 
+  * asar gozari hadaya 63
+  * andaze-haye chapi 75
+  * barcahsb ru yakhchali 81
+  * sherkat iranian magnet 15
+  * tarahi mazhabi 77
+  * tarahi rayegan 76
+  * kart visit 74
+  * magnet badane mashin 80
+  * magnet tablighati 79
+  * moafaghiat hadaya tablighati 64
+  * vaghe magnet 82
+  * vizhegi hadaya tablighati 62
+  */
