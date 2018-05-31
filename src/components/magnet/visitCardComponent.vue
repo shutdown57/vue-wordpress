868e3b8cf61add823b1210c82347ee29534f1132
majid mohamadi <m.shutdown57@gmail.com>
2018-05-31 13:33:50 +0430

    fixed, added and cleanup fixed product images, fixed product title, fixed comment infinite loading, fixed category number, added sleep and base64 functions, added base url and nonce, fixed loading issue

--- src/components/magnet/visitCardComponent.vue
+++ src/components/magnet/visitCardComponent.vue
@@ -11,7 +11,7 @@
             <div class="row">
                 <div v-for="product in productVisitCard" class="col-xs-6 col-md-3">
                     <a href="#" class="thumbnail" data-toggle="modal" data-target="#myModal">
-                    <img :src="product.img_info[0].url" :alt="product.img_info[0].title" v-model="img_info=product.img_info[0]">
+                    <img :src="product.img_info[0].url" alt="کارت ویزیت مگنتی" v-model="img_info=product.img_info[0]">
                     </a>
                 </div>
             </div>
@@ -24,10 +24,10 @@
         <div class="modal-content">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" dir="rtl" id="myModalLabel">{{img_info.title}}</h4>
+            <h4 class="modal-title" dir="rtl" id="myModalLabel">{{"کارت ویزیت مگنتی"}}</h4>
         </div>
         <div class="modal-body text-center">
-            <img class="img-responsive" :src="img_info.url" :alt="img_info.title">
+            <img class="img-responsive" :src="img_info.url" alt="کارت ویزیت مگنتی">
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">بستن</button>
@@ -37,25 +37,30 @@
     </div>
     
     <!-- Infinite Loading -->
-    <infinite-loading @infinite="infiniteHandler">
-        <span slot="no-more">
-            <div class="alert alert-warning alert-dismissible" role="alert" dir="rtl">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <strong>توجه!</strong> پست دیگری وجود ندارد.
-            </div>
-        </span>
-    </infinite-loading>
+    <!-- <div infinite-wrapper>
+        <infinite-loading @infinite="infiniteHandler" force-use-infinite-wrapper="true">
+            <span slot="no-more">
+                <div class="alert alert-warning alert-dismissible" role="alert" dir="rtl">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>توجه!</strong> پست دیگری وجود ندارد.
+                </div>
+            </span>
+        </infinite-loading>
+    </div> -->
+    {{infiniteHandler()}}
 </div>
 </template>
 
 <script>
 /**
- * @component: White Board
+ * @component: Visit Card
+ * Category: 91
  */
 import InfiniteLoadnig from 'vue-infinite-loading';
-
+import {sleep, Base64} from '../../mixins/utils';
 import api from '../../api';
-import {PRODUCT_WHITEBOURD} from '../../store/staticsCategories';
+import {PRODUCT_CARD_VISIT} from '../../store/staticsCategories';
+import { BASE_URL, NONCE } from '../../config';
 
 export default {
     name: 'whiteBoard',
@@ -66,6 +71,8 @@ export default {
 
     data() {
         return {
+            token: '',
+            counts: true,
             img_info: {},
             productVisitCard: [],
             msg: 'نمونه کار‌های کارت ویزیت',
@@ -83,40 +90,72 @@ export default {
 
     methods: {
         infiniteHandler($state) {
-            let CATEGORIES_OUT = [1, 2, 7, 5, 11, 16, 15, 14, 10, 12];
-            this.$http.get("http://wordpress.app/wp-json/wp/v2/posts", {
-                params: {
-                    categories: PRODUCT_CARD_SUBSCRIBE,
-                    categories_exclude: CATEGORIES_OUT,
-                    page: this.productVisitCard.length / 6 + 1,
-                    per_page: 6
-                }
-            }).then(res => {
-                if (res.body.length) {
-                    res.body.map((cur_img, i_img, arr_img) => {
-                        cur_img.img_info = [];
-                        api.getMediaId(cur_img.featured_media)
-                            .then(resolve => {
-                                cur_img.img_info.push({
-                                    title: resolve.body.title.rendered,
-                                    url: resolve.body.source_url
+            sleep(2000).then(() => {
+                let CATEGORIES_OUT = [83, 84, 85, 86, 87, 88, 89, 90, 92, 1, 61];
+                /*****************************************************************************************/
+                // Get number of posts
+                this.$http.get(BASE_URL + "wp-json/wp/v2/categories/" + PRODUCT_CARD_VISIT, {
+                            before: (request) => {
+                                request.headers.set('X-WP-Nonce', NONCE);
+                                request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+                                request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
+                            }
+                        }).then(resolve => {
+                            this.counts = resolve.body.count;
+                        }, reject => {});
+                /*****************************************************************************************/
+                // Get Posts
+                this.$http.get(BASE_URL + "wp-json/wp/v2/posts", {
+                    params: {
+                        categories: PRODUCT_CARD_VISIT,
+                        categories_exclude: CATEGORIES_OUT,
+                        page: parseInt(this.productVisitCard.length / 6) + 1,
+                        per_page: 6
+                    },
+                    before: (request) => {
+                        request.headers.set('X-WP-Nonce', NONCE);
+                        request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+                        request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
+                    }
+                }).then(res => {
+                    if (res.body.length && this.productVisitCard.length < this.counts) {
+                        res.body.map((cur_img, i_img, arr_img) => {
+                            cur_img.img_info = [];
+                            /*****************************************************************************/
+                            // Get post image
+                            this.$http.get(BASE_URL + "wp-json/info/v1/post",
+                            {
+                                params: {
+                                    post_id: cur_img.id
+                                },
+                                before: (request) => {
+                                    request.headers.set('X-WP-Nonce', NONCE);
+                                    request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+                                    request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
+                                }
+                            }).then(resp => {
+                                    cur_img.img_info.push({
+                                        url: resp.body.url
+                                    });
+                                }, reject => {
+                                    this.alert_msg.have = true;
+                                    this.alert_msg.msg = 'مشکل در ارتباط با سرور';
+                                    this.alert_msg.type = 'alert-danger';
                                 });
-                            }, reject => { 
-                                this.alert_msg.have = true;
-                                this.alert_msg.msg = 'مشکل در ارتباط با سرور';
-                                this.alert_msg.type = 'alert-danger';
-                             });
-                    });
-                    this.productVisitCard = this.productVisitCard.concat(res.body);
-                    $state.loaded();
-                    if (this.productVisitCard.length % 6 == 10) {
+                        });
+
+                        this.productVisitCard = [...res.body];
+                        $state.loaded();
+                        
+                        if (this.productVisitCard.length % 6 == this.counts) {
+                            $state.complete();
+                        }
+                    } else {
                         $state.complete();
                     }
-                } else {
+                }, rej => {
                     $state.complete();
-                }
-            }, rej => {
-                $state.complete();
+                });
             });
         }
     }
@@ -124,5 +163,6 @@ export default {
 </script>
 
 <style>
-
+.direction-rtl { direction: rtl; }
+img { width: 100% !important;}
 </style>
