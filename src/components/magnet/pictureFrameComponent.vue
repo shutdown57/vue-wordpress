965e7fe4a94d039eebb151eb78b197355e45db7c
majid mohamadi <m.shutdown57@gmail.com>
2018-06-10 12:13:51 +0430

    cleanup

--- src/components/magnet/pictureFrameComponent.vue
+++ src/components/magnet/pictureFrameComponent.vue
@@ -52,11 +52,9 @@
  * @component: Picture Frame
  * Category: 88
  */
-import InfiniteLoading from 'vue-infinite-loading';
 import {sleep, Base64} from '../../mixins/utils';
-import api from '../../api';
 import { PRODUCT_PICTURE_FRAME } from '../../store/staticsCategories';
-import { BASE_URL, NONCE } from '../../config';
+import { BASE_URL } from '../../config';
 
 export default {
     name: 'pictureFrame',
@@ -98,31 +96,31 @@ export default {
                     request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234' ));
                 }
             }).then(res => {
-                    res.body.map((cur_img, i_img, arr_img) => {
-                        cur_img.img_info = [];
-                        /*****************************************************************************/
-                        // Get post image
-                        this.$http.get(BASE_URL + "wp-json/info/v1/post",
-                        {
-                            params: {
-                                post_id: cur_img.id
-                            },
-                            before: (request) => {
-                                request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
-                                request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
-                            }
-                        }).then(resp => {
-                            cur_img.img_info.push({
-                                url: resp.body.url
-                            });
-                        }, reject => {
-                            this.alert_msg.have = true;
-                            this.alert_msg.msg = 'مشکل در ارتباط با سرور';
-                            this.alert_msg.type = 'alert-danger';
+                res.body.map((cur_img, i_img, arr_img) => {
+                    cur_img.img_info = [];
+                    /*****************************************************************************/
+                    // Get post image
+                    this.$http.get(BASE_URL + "wp-json/info/v1/post",
+                    {
+                        params: {
+                            post_id: cur_img.id
+                        },
+                        before: (request) => {
+                            request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+                            request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
+                        }
+                    }).then(resp => {
+                        cur_img.img_info.push({
+                            url: resp.body.url
                         });
+                    }, reject => {
+                        this.alert_msg.have = true;
+                        this.alert_msg.msg = 'مشکل در ارتباط با سرور';
+                        this.alert_msg.type = 'alert-danger';
                     });
+                });
 
-                    this.productPictureFrame = res.body.copyWithin();
+                this.productPictureFrame = res.body.copyWithin();
             }, rej => { /* $state.complete(); */ });
         },
 
