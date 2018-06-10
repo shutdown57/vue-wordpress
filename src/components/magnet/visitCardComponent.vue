63f4abc367653b9f5c9a6e3d476c84726afbc0fe
majid mohamadi <m.shutdown57@gmail.com>
2018-06-10 11:10:15 +0430

    fixed featured image and posts issues and cleanup

--- src/components/magnet/visitCardComponent.vue
+++ src/components/magnet/visitCardComponent.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="direction-rtl">
+    <div v-if="num < 1">{{infiniteHandler()}}{{loadPage()}}</div>
     <div v-if="alert_msg.have" class="alert text-center" :class="alert_msg.type" role="alert">
         {{ alert_msg.msg }}
     </div>
@@ -35,19 +36,14 @@
         </div>
     </div>
     </div>
+
+    <!-- Paginate -->
+    <div v-if="page_list.length > 1">
+        <span v-for="item in page_list" class="text-center">
+            <button type="button" class="btn btn-primary" @click="loadPage(item)">{{item}}</button>
+        </span>
+    </div>
     
-    <!-- Infinite Loading -->
-    <!-- <div infinite-wrapper>
-        <infinite-loading @infinite="infiniteHandler" force-use-infinite-wrapper="true">
-            <span slot="no-more">
-                <div class="alert alert-warning alert-dismissible" role="alert" dir="rtl">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <strong>توجه!</strong> پست دیگری وجود ندارد.
-                </div>
-            </span>
-        </infinite-loading>
-    </div> -->
-    {{infiniteHandler()}}
 </div>
 </template>
 
@@ -65,17 +61,13 @@ import { BASE_URL, NONCE } from '../../config';
 export default {
     name: 'whiteBoard',
 
-    components: {
-        InfiniteLoadnig
-    },
-
     data() {
         return {
-            token: '',
-            counts: true,
             img_info: {},
+            num: 0,
             productVisitCard: [],
             msg: 'نمونه کار‌های کارت ویزیت',
+            page_list: [],
             alert_msg: {
                 have: false,
                 msg: '',
@@ -89,73 +81,68 @@ export default {
     },
 
     methods: {
-        infiniteHandler($state) {
+        loadPage: function (page_number) {
+            page_number = parseInt(page_number);
+            let CATEGORIES_OUT = [83, 84, 85, 86, 87, 88, 89, 90, 92, 1, 61];
+            /*****************************************************************************************/
+            // Get Posts
+            this.$http.get(BASE_URL + "wp-json/wp/v2/posts", {
+                params: {
+                    categories: PRODUCT_CARD_VISIT,
+                    categories_exclude: CATEGORIES_OUT,
+                    page: page_number,
+                    per_page: 10
+                },
+                before: (request) => {
+                    request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+                    request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234' ));
+                }
+            }).then(res => {
+                res.body.map((cur_img, i_img, arr_img) => {
+                    cur_img.img_info = [];
+                    /*****************************************************************************/
+                    // Get post image
+                    this.$http.get(BASE_URL + "wp-json/info/v1/post", {
+                        params: {
+                            post_id: cur_img.id
+                        },
+                        before: (request) => {
+                            request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+                            request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
+                        }
+                    }).then(resp => {
+                        cur_img.img_info.push({
+                            url: resp.body.url
+                        });
+                    }, reject => {
+                        this.alert_msg.have = true;
+                        this.alert_msg.msg = 'مشکل در ارتباط با سرور';
+                        this.alert_msg.type = 'alert-danger';
+                    });
+                });
+                this.productVisitCard = res.body.copyWithin();
+            }, rej => {});
+        },
+
+        infiniteHandler: function ($state) { 
+            this.num += 1;
             sleep(2000).then(() => {
-                let CATEGORIES_OUT = [83, 84, 85, 86, 87, 88, 89, 90, 92, 1, 61];
                 /*****************************************************************************************/
                 // Get number of posts
-                this.$http.get(BASE_URL + "wp-json/wp/v2/categories/" + PRODUCT_CARD_VISIT, {
-                            before: (request) => {
-                                request.headers.set('X-WP-Nonce', NONCE);
-                                request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
-                                request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
-                            }
-                        }).then(resolve => {
-                            this.counts = resolve.body.count;
-                        }, reject => {});
-                /*****************************************************************************************/
-                // Get Posts
-                this.$http.get(BASE_URL + "wp-json/wp/v2/posts", {
-                    params: {
-                        categories: PRODUCT_CARD_VISIT,
-                        categories_exclude: CATEGORIES_OUT,
-                        page: parseInt(this.productVisitCard.length / 6) + 1,
-                        per_page: 6
-                    },
+                this.$http.get(BASE_URL + "wp-json/wp/v2/categories/" + PRODUCT_CARD_VISIT,{
                     before: (request) => {
-                        request.headers.set('X-WP-Nonce', NONCE);
                         request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
-                        request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
+                        request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234' ));
                     }
-                }).then(res => {
-                    if (res.body.length && this.productVisitCard.length < this.counts) {
-                        res.body.map((cur_img, i_img, arr_img) => {
-                            cur_img.img_info = [];
-                            /*****************************************************************************/
-                            // Get post image
-                            this.$http.get(BASE_URL + "wp-json/info/v1/post",
-                            {
-                                params: {
-                                    post_id: cur_img.id
-                                },
-                                before: (request) => {
-                                    request.headers.set('X-WP-Nonce', NONCE);
-                                    request.headers.set('Content-Type', 'application/x-www-form-urlencoded');
-                                    request.headers.set('Authorization', 'Basic ' + Base64.encode( 'default:strongPassword1234'));
-                                }
-                            }).then(resp => {
-                                    cur_img.img_info.push({
-                                        url: resp.body.url
-                                    });
-                                }, reject => {
-                                    this.alert_msg.have = true;
-                                    this.alert_msg.msg = 'مشکل در ارتباط با سرور';
-                                    this.alert_msg.type = 'alert-danger';
-                                });
-                        });
-
-                        this.productVisitCard = [...res.body];
-                        $state.loaded();
-                        
-                        if (this.productVisitCard.length % 6 == this.counts) {
-                            $state.complete();
-                        }
-                    } else {
-                        $state.complete();
+                }).then(resolve => {
+                    let total_page = parseInt(resolve.body.count / 10);
+                    if (resolve.body.count % 10 > 0) {
+                        total_page += 1;
                     }
-                }, rej => {
-                    $state.complete();
-                });
+                    for (let i = 1; i <= total_page; i++) {
+                        this.page_list.push(i);
+                    }
+                }, reject => {});
             });
         }
     }
